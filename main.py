fc1d6cb0da7127e7454e2176ce095c3359b6fa9f
ani-laliashvili <71911564+ani-laliashvili@users.noreply.github.com>
2022-03-31 20:13:50 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -20,15 +20,23 @@ turtle.shape(image)
 coor_data = pd.read_csv("50_states.csv")
 
 game_is_on = True
+score = 0
+guessed_states = []
 while game_is_on:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+    answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?")
+    if answer_state.lower() == 'exit':
+        all_states = coor_data.state.to_list()
+        states_to_learn = [i for i in all_states if i not in guessed_states]
+        states_to_learn = pd.DataFrame(states_to_learn)
+        states_to_learn.to_csv('states_to_learn.csv')
+        break
 
     answer_state = proper_input(answer_state)
-
-    if answer_state in coor_data['state'].to_list():
+    if answer_state in coor_data['state'].to_list() and answer_state not in guessed_states:
         state_on_map = Answer(int(coor_data[coor_data['state'] == answer_state]['x']), int(coor_data[coor_data['state'] == answer_state]['y']), answer_state)
-
-turtle.mainloop()
+        #if answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+        score += 1
 
 
 
